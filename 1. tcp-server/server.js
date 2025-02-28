10d673213a997308eed3850e9e2ed2b571d93462
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-28 20:08:26 +0530

    In this program understand the TCP connection by simulate in Wireshark using telent

---
+++ 1. tcp-server/server.js
@@ -0,0 +1,22 @@
+import net from "node:net";
+
+// Create a TCP server
+const server = net.createServer((socket) => {
+  console.log("Client connected", socket);
+
+  socket.on("data", (chunk) => {
+    console.log("Received:", chunk.toString());
+
+    socket.write(`Received: ${chunk}`);
+
+    socket.end();
+  });
+
+  socket.on("error", (err) => {
+    console.log(`Error: ${err}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
